2f09abd2ab1acda5e4c5879517ef9c9a199e5882
hzauggj <279103446@qq.com>
2022-01-03 12:19:34 +0800

    Hello World

---
+++ javalearn/src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @Description
+ * @Author 程序员小猪
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2022/1/3 0003
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello Git Hub ");
+    }
+}
